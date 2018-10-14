aac0f56672de992611fbfb5665553957ab943a47
batuhanduzgun <batuhan.duzgun@sahibinden.com>
2018-10-14 14:20:26 +0300

    Presentation PDF added!
    Change-Id: I7d02999f908c85e45e50a364dd2660b75af0a806

--- com.personal.book.library/src/main/java/com/personal/book/library/datalayer/config/jpa/RepositorySpringConfiguration.java
+++ com.personal.book.library/src/main/java/com/personal/book/library/datalayer/config/jpa/RepositorySpringConfiguration.java
@@ -7,7 +7,6 @@ import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
